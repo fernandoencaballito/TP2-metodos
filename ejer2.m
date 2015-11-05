c856f9a5baafc021afffb6f2ea4f6fc72a8418a6
fernandoencaballito <fernando.en.caballito@gmail.com>
2015-11-04 22:06:41 -0300

    Update ejer2.m
    funciona en windows con matlab

--- ejer2.m
+++ ejer2.m
@@ -1,25 +1,25 @@
-%% codigo octave
+%% codigo matlab
 
-AUDIO_DIRECTORY="./sounds/";%% directorio en el que estan los archivos de audio.
-FILE_PATTERN="*.wav";%%patron para buscar archivos wav
-
-
-
-files = readdir (AUDIO_DIRECTORY);
+AUDIO_DIRECTORY='sounds\';%% directorio en el que estan los archivos de audio.
+FILE_PATTERN='*.wav';%%patron para buscar archivos wav
 
 COMPLETE_PATTERN=strcat(AUDIO_DIRECTORY,FILE_PATTERN);
-files=glob(COMPLETE_PATTERN);
+
+files=dir(COMPLETE_PATTERN);
+
+%files=glob(COMPLETE_PATTERN);
 
 for i=1:length(files)
-	file_name=files(i)
+	file=files(i);
+    file_name=strcat(AUDIO_DIRECTORY,file.name)
 	
 	
-	[x, fs] = auload(file_in_loadpath(file_name));
+	[x, fs] = wavread(file_name);
 	
 	x=x(:,1);%% si el audio es estereo (dos canales), se descarta el segundo canal.
 
-%%ACA HACER LO QUE HAGA FALTA CON LOS DATOS DEL ARCHIVO DE AUDIO QUE ESTAN EN X, EN FS frecuencia de muestreo.
+    %%TRABAJAR CON EL SONIDO EN x.
 	sound(x,fs);%se lo reproduce
 	
 	
-endfor
+end
